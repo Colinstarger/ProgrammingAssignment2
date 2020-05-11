303f34fbe9d040290b2a1ba4c0b53b394760ea1b
Colin Starger <colinstarger@gmail.com>
2020-05-11 10:25:13 -0400

    Followed the model from the readme, but substituted inverse for mean and used Solve instead of Mean

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
+## Colin's Cachematrix
 ## functions do
 
-## Write a short comment describing this function
+## Based on the example makeVector
+## I think it works, but there is no test suite
+## But used one I found in the discussion forum and it seemed to work!
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) m <<- inverse
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+  
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  message("calculating...")
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
+  
+  }
